15fd9f4a033187c547bd9a26c721f291609b4c0c
Frederick Roegiers <frederick@rogerthat.be>
2024-02-12 10:55:22 +0100

    wip

--- APP/src/app.js
+++ APP/src/app.js
@@ -1,1 +1,14 @@
 console.log("Hello World!");
+
+const myself = {
+  firstname: "Corsair",
+  lastname: "",
+  nickname: "Corsair",
+  classname: "Marvel Comics",
+  email: "corsair@superhero.com",
+  age: 0,
+  avatar: "https://www.superherodb.com/pictures2/portraits/10/100/49.jpg",
+  motto: "Starjammers (leader), formerly N.A.S.A, U.S.A.F.",
+  about: "The Starjammer, Mobile",
+  hobbies: '"Pirate, former pilot"',
+};
