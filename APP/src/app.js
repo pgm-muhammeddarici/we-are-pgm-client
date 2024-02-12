ab80a8f4c24328761866d8e53bed162f5dd92ee4
Frederick Roegiers <frederick@rogerthat.be>
2024-02-12 11:29:55 +0100

    wip

--- APP/src/app.js
+++ APP/src/app.js
@@ -9,10 +9,9 @@ const myself = {
   lastname: "Roegiers",
   nickname: "F-Rogers",
   classname: "1PGM-a",
-  email: "frederick.roegiers@arteveldehs.be",
+  email: "frederick2.roegiers@arteveldehs.be",
   age: 33,
-  avatar:
-    "https://scontent-bru2-1.xx.fbcdn.net/v/t39.30808-6/386604607_10229212391667899_2097486288866084778_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=tftoh3i6KMoAX_y2pKR&_nc_ht=scontent-bru2-1.xx&oh=00_AfCcY4SFNgw11kxEF3mU9nRAdiC0jAQz1GTqtLq3OmmAgw&oe=65CF585D",
+  avatar: "https://picsum.photos/536/354",
   motto: "I can be badass, sometimes I eject USB without removing safely",
   about: "Sometimes I dream in binary code.",
   hobbies: ["Tuinieren", "Programmeren", "Going out in the psytrance scene"],
@@ -39,7 +38,7 @@ const createStudent = async () => {
 // initialisation
 const init = async () => {
   // create a student
-  const student = createStudent();
+  const student = await createStudent();
   // log in console
   console.log(student);
 };
